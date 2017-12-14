37c06bf486b74a404ee58e86e3690befb6324011
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-14 12:59:00 -0200

    atualizado conteudo dos scripts e estilos para validação dos formularios do modal e de contato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-setTimeout(apresentarModal, 5000);
+setTimeout(apresentarModal, 1000);
 
 function apresentarModal() {
     var modal = document.querySelector('.modal');
@@ -13,11 +13,18 @@ function apresentarModal() {
     }
 }
 
+
+// formulario do modal
 if (document.forms['modalForm']) {
     //    validação
     var form = document.forms['modalForm'];
     
     form.addEventListener('submit', validarFormModal);
+    
+     form.email.addEventListener('keyup', function() {
+         form.email.className = '';
+         document.querySelector('span.invalido').style.display = 'none';
+     });
 }
 
 function validarFormModal(evt) {
@@ -25,11 +32,96 @@ function validarFormModal(evt) {
 
     var email = form.email.value;
     
-    if(email !== '' && email.indexOf('@') > 0 && email.indexOf('.') > email.indexOf('@')) {
+    if(validarEmail(email)) {
         alert('Email válido!');
-    } else {
-        alert('Email inválido!');
+    } else {        
+        form.email.className = 'invalido';
+        document.querySelector('span.invalido').style.display = 'block';
         
         evt.preventDefault();
+    }
+}
+
+
+//formulario de contato
+
+if (document.forms['contatoForm']) {
+    //    validação
+    var form = document.forms['contatoForm'];
+    
+    form.addEventListener('submit', validarFormContato);
+
+}
+
+function validarFormContato(evt) {
+    var form = document.forms['contatoForm'];
+    
+    var formValido = true;
+    
+    
+    if(!naoVazio(form.nomeCompleto.value)) {
+        form.nomeCompleto.className = 'invalido';
+        formValido = false;
+    }
+
+    if(!naoVazio(form.telefone.value)) {
+        form.telefone.className = 'invalido';
+        formValido = false;
+    }
+
+    if(!naoVazio(form.mensagem.value)) {
+        form.mensagem.className = 'invalido';
+        formValido = false;
+    }
+
+    if(!validarEmail(form.email.value)) {
+        form.email.className = 'invalido';
+        formValido = false;
+    }
+
+    if(!formValido) {
+        evt.preventDefault();
+    }
+    
+    
+    var inputs = document.querySelectorAll('form[name=contatoForm] input[type=text]');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('keypress', function(){
+        this.className = '';
+        })
+    }
+    
+    var textarea = document.querySelector('form[name=contatoForm] textarea');
+        textarea.addEventListener('keyup', function(){
+        this.className = '';
+            
+        document.querySelector('.qtdCaractere').innerHTML = 'Caractere(s): ' + this.value.length;
+    })
+    
+}
+
+
+
+
+
+
+/* funções genéricas */
+
+function validarEmail(email) {
+    if(
+    email !== '' && email.indexOf('@') > 0 && email.indexOf('.') > email.indexOf('@')
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function naoVazio(texto) {
+    console.log(texto);
+    if(texto.trim().length > 0) {
+        return true;
+    } else {
+        return false;
     }
 }
